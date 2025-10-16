1e9a4bdfc8528e1499906b7eb8689eb7433329e2
Sxngwk <sxngwk>
2025-10-16 19:54:12 +0900

    v0.3 Pokemon game

--- week6/src/GameDemo.java
+++ week6/src/GameDemo.java
@@ -2,7 +2,9 @@ public class GameDemo {
     public static void main(String[] args) {
         Pikachu mypikachu = new Pikachu(100);
         Pikachu yourpikachu = new Pikachu(100);
+        Sqiurtle mySqiurtle = new Sqiurtle(100);
         System.out.println("Game Start~");
         mypikachu.attack(yourpikachu);
+        mySqiurtle.attack(yourpikachu);
     }
 }

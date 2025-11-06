ec0f024526fe6f4e8b812bf6938000c0d543b12b
Sxngwk <sxngwk>
2025-11-06 19:28:17 +0900

    collection v0.1

---
+++ week09/src/Pokemons/Pokemon.java
@@ -0,0 +1,56 @@
+package Pokemons;
+
+import fly.Flyable;
+
+public abstract class Pokemon {
+    private String name;
+    private int hp;
+    private int attackPower;
+    protected Flyable flyingtool; // has-a (Aggregation)
+
+    public Pokemon(String name, int hp, int attackPower, Flyable flyingtool){
+        this.name = name;
+        this.hp = hp;
+        this.attackPower = attackPower;
+        this.flyingtool = flyingtool;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getHp(){
+        return hp;
+    }
+
+    public int getAttackPower(){
+        return attackPower;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    public void setFlyingtool(Flyable flyingtool) {
+        this.flyingtool = flyingtool;
+    }
+
+    public void performFlyable() {
+        this.flyingtool.fly(this);
+    }
+
+    public abstract void attack(Pokemon target);
+    public void receiveDamage(int damage){
+        hp = hp - damage;
+        if(hp < 0) hp = 0;
+    }
+
+    public boolean isFainted(){
+        return hp <= 0;
+    }
+
+    @Override
+    public String toString() {
+        return name + " (HP : " + hp + ", AttackPower : " + attackPower + ")";
+    }
+}
---
+++ week09/src/fly/Flyable.java
@@ -0,0 +1,9 @@
+package fly;
+
+import Pokemons.Pokemon;
+
+public interface Flyable {
+//    abstract void fly();
+//    void fly();
+    void fly(Pokemon pokemon);
+}

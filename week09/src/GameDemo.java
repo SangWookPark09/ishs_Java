df444e0d35e0c347046f0056eac27fa93d793d22
Sxngwk <sxngwk>
2025-11-06 20:18:40 +0900

    v1.5 Pokemon game

--- week09/src/GameDemo.java
+++ week09/src/GameDemo.java
@@ -1,7 +1,4 @@
-import Pokemons.Charizard;
-import Pokemons.Pikachu;
-import Pokemons.Pokemon;
-import Pokemons.Sqiurtle;
+import Pokemons.*;
 import fly.NoFly;
 import fly.Wings;
 
@@ -12,6 +9,23 @@ public class GameDemo {
     public static void main(String[] args) {
 //        int randomNumber = (int)(Math.random() * 3); //0 ~ 2
 //        System.out.println(randomNumber);
+        Skill[] pikachuSkills = {
+                new Skill("Nuzzle", 20),
+                new Skill("ThunderShock", 40),
+                new Skill("Quick Attack", 40)
+        };
+
+        Skill[] sqiurtleSkills = {
+                new Skill("Tackle", 40),
+                new Skill("Water gun", 40),
+                new Skill("Rapid Spin", 50)
+        };
+
+        Skill[] charizardSkills = {
+                new Skill("Scratch", 40),
+                new Skill("Dragon Breath", 60),
+                new Skill("Flare Blitz", 120)
+        };
 
         Pokemon playerPokemon = null;
         Scanner scanner = new Scanner(System.in);
@@ -20,13 +34,13 @@ public class GameDemo {
                 System.out.print("플레이어 포켓몬스터 선택\n1) 피카츄 2) 꼬부기 3) 리자몽 : ");
                 int number = scanner.nextInt();
                 if (number == 1) {
-                    playerPokemon = new Pikachu(100, 27, new NoFly());
+                    playerPokemon = new Pikachu(100, 27, new NoFly(), pikachuSkills);
                     break;
                 } else if (number == 2) {
-                    playerPokemon = new Sqiurtle(120, 21, new NoFly());
+                    playerPokemon = new Sqiurtle(120, 21, new NoFly(), sqiurtleSkills);
                     break;
                 } else if (number == 3) {
-                    playerPokemon = new Charizard(200, 40, new Wings());
+                    playerPokemon = new Charizard(200, 40, new Wings(), charizardSkills);
                     break;
                 } else {
                     System.out.println("메뉴에서 골라주세요");
@@ -42,13 +56,13 @@ public class GameDemo {
         Pokemon enemyPokemon;
         System.out.println("야생의 포켓몬이 나타났습니다!");
         if(randomNumber == 0) {
-            enemyPokemon = new Pikachu(100, 27, new NoFly());
+            enemyPokemon = new Pikachu(100, 27, new NoFly(), pikachuSkills);
         }
         else if (randomNumber == 1) {
-            enemyPokemon = new Sqiurtle(120, 21, new NoFly());
+            enemyPokemon = new Sqiurtle(120, 21, new NoFly(), sqiurtleSkills);
         }
         else if (randomNumber == 2) {
-            enemyPokemon = new Charizard(200, 40, new Wings());
+            enemyPokemon = new Charizard(200, 40, new Wings(), charizardSkills);
         }
 
 //        Pokemons.Pokemon playerPokemon = new Pokemons.Pikachu(100, 27, new fly.NoFly());
